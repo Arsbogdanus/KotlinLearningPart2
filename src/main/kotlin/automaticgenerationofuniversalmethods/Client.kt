5807f346895c6ac3cbca1ce55a635fd797414145
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2025-07-08 11:55:14 +0300

    Automatic generation of universal methods

---
+++ src/main/kotlin/automaticgenerationofuniversalmethods/Client.kt
@@ -0,0 +1,7 @@
+package org.example.automaticgenerationofuniversalmethods
+
+class Client(val name: String, val postalCode: Int) {
+    fun copy(name: String = this.name,
+             postalCode: Int = this.postalCode) =
+        Client(name, postalCode)
+}
