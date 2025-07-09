257e7f4b2dcf7bd3cd0d1e5707c6686966371866
Bogdan Zhuravlov <arsbogdanus@gmail.com>
2025-07-09 13:56:09 +0300

    Delegation in classes. Keyword by

---
+++ src/main/kotlin/delegationinclasseskeywordby/CountingSet.kt
@@ -0,0 +1,21 @@
+package org.example.delegationinclasseskeywordbuy
+
+import addingmethodstothirdpartyclasses.c
+
+class CountingSet<T>(
+    val innerSet: MutableCollection<T> = HashSet<T>()
+) : MutableCollection<T> by innerSet {                 // Делегирование реализации MutableCollection объекту в поле innerSet
+    var objectsAdded = 0
+
+
+    // Собственная реализация вместо делегирования ---->
+    override fun add(element: T): Boolean {
+        objectsAdded++
+        return innerSet.add(element)
+    }
+
+    override fun addAll(c: Collection<T>): Boolean {
+        objectsAdded += c.size
+        return innerSet.addAll(c)
+    }
+}
